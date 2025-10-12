4570c56800d74866e036d2f571cf4c8799d4c4b1
Mikhail Moskovkin <kontadorlol@gmail.com>
2025-10-12 13:04:11 +0300

    welcome page with login and clipRrect

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/views/widget_tree.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -7,8 +8,30 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Welcome')),
-      body: Column(
-        children: [ClipRRect(child: Image.asset('assets/images/os.jpeg'))],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(40.0),
+              child: Image.asset('assets/images/os.jpeg'),
+            ),
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
